71822e07cf8cc4d4957967d1c0dcd45d157799e5
Mahaalotaibi97 <mahaalotaibi888@gmail.com>
2022-03-10 09:39:36 +0300

    skills map doctype added

---
+++ skill_app/skills_module/doctype/employee_skills_map/employee_skills_map.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, maha and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Employee Skills Map', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ skill_app/skills_module/doctype/skills_map/skills_map.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, maha and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('skills map', {
+	// refresh: function(frm) {
+
+	// }
+});
